c07377366545886e2bda79efb6ed7e3f48a30b23
rech99 <rech_99@hotmail.com>
2019-08-27 12:44:21 -0700

    super cambios

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const buttons = document.querySelectorAll('button');
 
 const playnote = event =>{
-const button = event.target;
-const note =  button.dataset.note;
-console.log('audio' + note);
-
+    const button = event.target;
+    const note = button.dataset.note;
+    const audioId = `audio${note}`;
+    //console.log(audioId);
+    const audio = document.getElementById(audioId);
+    console.log(audio);
+    audio.pause();
+    audio.currentTime = 0;
+    audio.play();
 }
 
 buttons.forEach(
-    button => button.addEventListener('click', func )
+    button => button.addEventListener('click', playnote )
 );
